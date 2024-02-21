04926a83ddb5643353a639bc17415376e570f19d
Eduardo Pimentel <125682114+Edupimentel007@users.noreply.github.com>
2024-02-21 13:14:02 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,35 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+return (
+<View style={styles.container}>
+<View style={{ backgroundColor: 'gray', width: '100%', height: 60 }}>
+</View>
+<View style={styles.secondaryContainer}>
+
+<Text style= {{color: 'blue',  fontWeight: 'bold', fontSize: 30, textAlign: 'right'}}>Eduardo Gramignolli Pimentel</Text>
+<Text style= {{color: 'blue',  fontWeight: 'italic', fontSize: 25, borderWidth: 1, borderColor: 'red' }}>Notebook 13</Text>
+<Text style= {{color: 'blue',  fontSize: 20, textAlign: 'center', backgroundColor: 'pink'}}>7</Text>
+<Text style= {{color: 'blue',  fontSize: 15, textAlign: 'center', backgroundColor: 'pink', borderWidth: 1, borderColor: 'blue', backgroundColor: 'yellow'}}>text</Text>
+
+</View>
+<View style={{ backgroundColor: 'gray', width: '100%', height: 60 }}>
+</View>
+</View>
+);
+}
+
+const styles = StyleSheet.create({
+container: {
+flex: 1,
+
+
+},
+secondaryContainer: {
+flex: 1,
+width: '100%',
+justifyContent: 'center',
+backgroundColor: '#fff',
+},
+});
